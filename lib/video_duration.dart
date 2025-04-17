93723e335496d6f64c6e432a44da0ee21cc5d852
fractale <2981437+stephane-archer@users.noreply.github.com>
2025-04-17 21:34:55 +0200

    add library code

--- lib/video_duration.dart
+++ lib/video_duration.dart
@@ -1,5 +1,17 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:thumblr/thumblr.dart';
+
+/// Returns the duration of the video in seconds as a double.
+/// Example: 3.728 means 3 seconds and 728 milliseconds.
+Future<double> getVideoDuration(String videoPath) async {
+  if (!FileSystemEntity.isFileSync(videoPath)){
+    throw ArgumentError("$videoPath is not a file", "videoPath");
+  }
+  Thumbnail thumbnail = await generateThumbnail(filePath: videoPath);
+  final videoDuration = thumbnail.videoDuration;
+  if (videoDuration == null) {
+    throw StateError("Video duration not found");
+  }
+  return videoDuration; // in seconds
 }
